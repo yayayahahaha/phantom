e373254071eb71bd8e8a5882d2d7d18593442823
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-02 10:53:24 +0800

    add url variable and onConsoleMessage callback function

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-var page = require('webpage').create();
-page.open('http://example.com', function(status) {
+var page = require('webpage').create(),
+	system = require('system'),
+	time = Date.now(),
+	// url = "https://dartnote.com/posts";
+	url = "http://localhost:3000";
+
+//this listenter could make javascript console.log in web show on terminal!
+page.onConsoleMessage = function(msg) {
+	console.log(msg);
+};
+
+// main javascrpit part
+page.open(url, function(status) {
 	console.log("Status: " + status);
 	if (status === "success") {
 		page.render('images/example.png');
